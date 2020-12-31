5e11be879fa670739946a01a0eba3de0b60de9a7
momipochi <40135161+momipochi@users.noreply.github.com>
2020-12-31 12:57:28 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -33,6 +33,8 @@ var x = setInterval(function() {
   
 if (distance > 0) {
 background(255, 0, 0);
+  textSize(50);
+  text("wait till it reaches 0..",50,50);
   textSize(40);
   text(days+" :",width/2-175,height/2)
   text(hours+" :",width/2-70,height/2);
